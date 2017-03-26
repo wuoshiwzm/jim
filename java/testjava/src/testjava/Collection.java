81184af9c71334d69db2213dfe69d33ead948179
wuoshiwzm <wuoshiwzm@hotmail.com>
2017-03-26 22:11:51 +0800

    3-26

---
+++ java/testjava/src/testjava/Collection.java
@@ -0,0 +1,41 @@
+package testjava;
+
+public class Collection <AnyType>{
+	
+	private AnyType object;
+
+	public int isEmpty(){
+		 return 1;
+	}
+	
+	public void makeEmpty(){
+		 
+	}
+	
+	public void insert(AnyType x){
+		object = x;
+		
+		System.out.print(object);
+	}
+	
+	public void remove(AnyType x){
+		
+	}
+	
+	public int equals(AnyType x, AnyType y){
+		return 1;
+	}
+	
+	public void isPresent(AnyType x){
+		
+	}
+	
+	
+	public static void main(String[] args) {
+	
+	}
+	
+	
+}
+	
+ 
--- java/testjava/src/testjava/test03_24_00.java
+++ java/testjava/src/testjava/test03_24_00.java
@@ -1,14 +1,40 @@
 package testjava;
 
 
+
 public class test03_24_00 {
 
 	public static void main(String[] args) {
-		permute("abdc");
+		
+		
+		long startTime=System.currentTimeMillis();   //获取开始时间  
+		fib(40000000);  //测试的代码段  
+		long endTime=System.currentTimeMillis(); //获取结束时间  
+		System.out.println("  ");
+		System.out.println("程序运行时间： "+(startTime-endTime)+"ms");   
+		
+	
+
+		
+	}
+	
+	//计算斐波那数
+	
+	public static void fib(int num){
+	int[] fibArr = new int[num];
+	fibArr[0] = 1;
+	fibArr[1] = 1;
+		
+		for(int i=2;i<num;i++){
+			fibArr[i] = fibArr[i-1]+fibArr[i-2];
+		}
+		
+		System.out.println(fibArr[num-1]);
+ 
 	}
 	
 	
-	public  void permute(String str){
+	public static void permute(String str){
 		
 	}
 	
@@ -25,4 +51,7 @@ public class test03_24_00 {
 			return ones(n/2)+1;
 		}
 	}
+
+
+
 }
